3edc004d6bd1a4b246942f70f73ac5e732bdcb00
Max Rack <maxwell.rack@gmail.com>
2023-08-15 13:53:25 -0400

    updated inputs excluding method and did some css

--- App.js
+++ App.js
@@ -27,15 +27,18 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    //this should be better lol
+    buttonInputs.Reference = "GRCh38"
+
+    //this should be better
     //check if typeInputs, selectInputs and buttonInputs are filled
     if(typeInputs.ID === undefined || typeInputs.Method === undefined || typeInputs.OpName === undefined || 
       buttonInputs.Report === undefined || buttonInputs.Sample === undefined || buttonInputs.Variant === undefined || buttonInputs.Reference === undefined || 
       selectInputs.Panel === undefined || selectInputs.Drug === undefined || selectInputs.Path === undefined ||
       selectInputs.Panel === "" || selectInputs.Drug === "" || selectInputs.Path === ""){
-      alert("Please fill in all the fields")
+      alert("Please fill in all the fields.")
       return;
     }
+
 
     console.log(typeInputs)
     console.log(selectInputs)
@@ -54,76 +57,110 @@ function App() {
          onChange={handleChangeType} />
 
         <div value={buttonInputs.Report}> 
-          <label>Report Type: </label>
-          <input type="radio" value="Basic" name="Report" 
+          <label>Report Type: </label> <br/>
+          <input type="radio" value="Variants" name="Report" 
+            onChange={handleChangeButton} /> Variants <br/>
+          <input type="radio" value="ClinInterp" name="Report"  
+            onChange={handleChangeButton} /> Variants and Clinical Interpretation
+
+          {/* <input type="radio" value="Basic" name="Report" 
             onChange={handleChangeButton} /> Basic
           <input type="radio" value="Drugs" name="Report"  
             onChange={handleChangeButton} /> Drugs summary
           <input type="radio" value="Genes" name="Report" 
             onChange={handleChangeButton} /> Genes summary
           <input type="radio" value="Final" name="Report" 
-            onChange={handleChangeButton} /> Final summary
+            onChange={handleChangeButton} /> Final summary */}
         </div>
 
         <div value={buttonInputs.Sample}> 
-          <label>Sample Type: </label>
+          <label>Sample Type: </label> <br/>
           <input type="radio" value="bam" name="Sample" 
-            onChange={handleChangeButton} /> bam
+            onChange={handleChangeButton} /> bam <br/>
           <input type="radio" value="vcf" name="Sample"  
             onChange={handleChangeButton} /> vcf
         </div>
+        
+        {/* TODO */}
+        <div>
+          <label>Method: </label>
+          <input 
+          type="text"
+          name="Method"
+          value={typeInputs.Method || ""}
+          onChange={handleChangeType} />
+        </div>
+        
+        <div>
+          <label> Pgx gene panel: </label>
+            <select name="Panel" value={selectInputs.Panel} onChange={handleChangeSelect}>
+              <option value=""></option>
+              <option value="COMT">COMT</option>
+              <option value="ABCD">ABCD</option>
+            </select>
+        </div>
 
-        <label>Method: </label>
-         <input 
-         type="text"
-         name="Method"
-         value={typeInputs.Method || ""}
-         onChange={handleChangeType} />
-
-        <label> Pgx gene panel </label>
-          <select name="Panel" value={selectInputs.Panel} onChange={handleChangeSelect}>
-            <option value=""></option>
-            <option value="COMT">COMT</option>
-            <option value="ABCD">ABCD</option>
-          </select>
-
-        <label> Drug </label>
-          <select name="Drug" value={selectInputs.Drug} onChange={handleChangeSelect}>
-            <option value=""></option>
-            <option value="All drugs">All drugs</option>
-            <option value="Some drugs">Some drugs</option>
-          </select>
+        <div>
+          <label> Drug: </label>
+            <select name="Drug" value={selectInputs.Drug} onChange={handleChangeSelect}>
+              <option value=""></option>
+              <option value="All drugs">All drugs</option>
+              <option value="Some drugs">Some drugs</option>
+            </select>
+        </div>
         
         <div value={buttonInputs.Variant}> 
-          <label>Variant Type: </label>
-          <input type="radio" value="actionable" name="Variant" 
-            onChange={handleChangeButton} /> Actionable
-          <input type="radio" value="potential" name="Variant"  
-            onChange={handleChangeButton} /> Potential
+          <label>Variant Type: </label> <br/>
+          <input type="radio" value="Actionable" name="Variant" 
+            onChange={handleChangeButton} /> Actionable <br/>
+          <input type="radio" value="All" name="Variant"  
+            onChange={handleChangeButton} /> All Relevant
         </div>
 
         <div value={buttonInputs.Reference}> 
-          <label>Reference: </label>
-          <input type="radio" value="GRCh37" name="Reference" 
-            onChange={handleChangeButton} /> GRCh37
-          <input type="radio" value="GRCh38" name="Reference"  
+          <label>Reference: </label> <br/>
+          <input type="radio" value="GRCh38" name="Reference" checked={true} 
             onChange={handleChangeButton} /> GRCh38
         </div>
+        
+        <div>
+          <label> Input path: </label>
+            <select name="Path" value={selectInputs.Path} onChange={handleChangeSelect}>
+              <option value=""></option>
+              <option value="Path 1">Path 1</option>
+              <option value="Path 2">Path 2</option>
+            </select>
+        </div>
 
-        <label> Input path </label>
-          <select name="Path" value={selectInputs.Path} onChange={handleChangeSelect}>
-            <option value=""></option>
-            <option value="Path 1">Path 1</option>
-            <option value="Path 2">Path 2</option>
-          </select>
+        <div>
+          <label>Operator Name: </label>
+          <input 
+          type="text"
+          name="OpName"
+          value={typeInputs.OpName || ""}
+          onChange={handleChangeType} />
+        </div>
 
-        <label>Operator Name: </label>
-         <input 
-         type="text"
-         name="OpName"
-         value={typeInputs.OpName || ""}
-         onChange={handleChangeType} />
+        <div>
+          <label>Operator Institution: </label>
+          <input 
+          type="text"
+          name="OpInst"
+          value={typeInputs.OpInst || ""}
+          onChange={handleChangeType} />
+        </div>
 
+        <div>
+          <label>Institution Address: </label>
+          <input 
+          type="text"
+          name="InstAddr"
+          value={typeInputs.InstAddr  || ""}
+          onChange={handleChangeType} />
+        </div>
+        
+        <br/>
+        
         <input type="submit"/>
       </form>
     </div>
@@ -131,18 +168,3 @@ function App() {
 }
 
 export default App;
-
-{/* <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.afds
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */} 
