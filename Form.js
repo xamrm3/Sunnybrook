91f7de5fee3ad07dd52dde07af788a22ec817ea3
Max Rack <maxwell.rack@gmail.com>
2023-08-24 17:12:04 -0400

    added sx styling to form

--- Form.js
+++ Form.js
@@ -59,17 +59,17 @@ function Form() {
 
       <h2>Pharmacogenetics Testing</h2>
 
-      <div className="inputs">
+      <div className="formInputs">
         <form onSubmit={handleSubmit}>
           <div>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <FormControl sx={{ mt: 2, width: 65/100 }} variant="outlined">
               <InputLabel htmlFor="SID">Sample ID</InputLabel>
               <OutlinedInput id="SID" type="text" name="ID" label="Sample ID" value={inputs.ID || ""} onChange={handleChange} onBlur={trimOnBlur}/>
             </FormControl>
           </div>
 
           <div>
-            <FormControl>
+            <FormControl sx={{ mt: 2 }}>
               <FormLabel id="Report">Report Type:</FormLabel>
               <RadioGroup name="Report" value={inputs.Report || ""} onChange={handleChange}>
                 <FormControlLabel value="Variants" control={<Radio />} label="Variants" />
@@ -79,7 +79,7 @@ function Form() {
           </div>
 
           <div>
-            <FormControl>
+            <FormControl sx={{ mt: 2 }}>
               <FormLabel id="Sample">Report Type:</FormLabel>
               <RadioGroup name="Sample" value={inputs.Sample || ""} onChange={handleChange}>
                 <FormControlLabel value="BAM" control={<Radio />} label="BAM" />
@@ -100,7 +100,7 @@ function Form() {
           </datalist> */}
          
           <div>
-            <FormControl sx={{ m: 1, minWidth: '25ch' }}>
+            <FormControl sx={{ mt: 2, width: 65/100 }}>
               <InputLabel id="Method">Method</InputLabel>
               <Select labelId="Method" value={inputs.Method || ""} name="Method" autowidth="true" onChange={handleChange} label="Method">
                 <MenuItem value="Illumina Short Read">Illumina Short Read</MenuItem>
@@ -113,15 +113,15 @@ function Form() {
           </div>
           
           <div>
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <FormControl sx={{ mt: 2, width: 65/100 }} variant="outlined">
             <InputLabel htmlFor="otherMethod">Other Method</InputLabel>
             <OutlinedInput id="otherMethod" type="text" name="otherMethod" label="Other Method" disabled={inputs.Method === "Other" ? false : true} value={inputs.otherMethod || ""} onChange={handleChange} onBlur={trimOnBlur}/>
-            <FormHelperText>If Other is selected in the above Method, please type here</FormHelperText>
+            <FormHelperText>If Other is selected for the above Method, please enter the method here</FormHelperText>
           </FormControl>
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, minWidth: '25ch' }}>
+            <FormControl sx={{ mt: 2, width: 65/100 }}>
               <InputLabel id="PGP">Pgx Gene Panel</InputLabel>
               <Select labelId="PGP" value={inputs.Panel || ""} name="Panel" autowidth="true" onChange={handleChange} label="Pgx Gene Panel">
                 <MenuItem value="COMT">COMT</MenuItem>
@@ -131,7 +131,7 @@ function Form() {
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, minWidth: '25ch' }}>
+            <FormControl sx={{ mt: 2, width: 65/100 }}>
               <InputLabel id="Drug">Drug</InputLabel>
               <Select labelId="Drug" value={inputs.Drug || ""} name="Drug" autowidth="true" onChange={handleChange} label="Drug">
                 <MenuItem value="All Drugs">All Drugs</MenuItem>
@@ -141,7 +141,7 @@ function Form() {
           </div>
 
           <div>
-            <FormControl>
+            <FormControl sx={{ mt: 2 }}>
               <FormLabel id="Variant">VariantType:</FormLabel>
               <RadioGroup name="Variant" value={inputs.Variant || ""} onChange={handleChange}>
                 <FormControlLabel value="Actionable" control={<Radio />} label="Actionable" />
@@ -151,7 +151,7 @@ function Form() {
           </div>
 
           <div>
-            <FormControl>
+            <FormControl sx={{ mt: 2 }}>
               <FormLabel id="Reference">Reference:</FormLabel>
               <RadioGroup name="Reference" value={inputs.Reference || ""} onChange={handleChange}>
                 <FormControlLabel value="GRCh38" control={<Radio />} checked={true} label="GRCh38" />
@@ -160,9 +160,9 @@ function Form() {
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, minWidth: '25ch' }}>
+            <FormControl sx={{ mt: 2, width: 65/100 }}>
               <InputLabel id="InputPath">Input Path</InputLabel>
-              <Select labelId="InputPath" value={inputs.Path || ""} name="Path" autowidth="true" onChange={handleChange} label="Input Path">
+              <Select labelId="InputPath" value={inputs.Path || ""} name="Path" onChange={handleChange} label="Input Path">
                 <MenuItem value="Path 1">Path 1</MenuItem>
                 <MenuItem value="Path 2">Path 2</MenuItem>
               </Select>
@@ -170,21 +170,21 @@ function Form() {
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <FormControl sx={{ mt: 2, width: 65/100 }} variant="outlined">
               <InputLabel htmlFor="ON">Operator Name</InputLabel>
               <OutlinedInput id="ON" type="text" name="OpName" label="Operator Name" value={inputs.OpName || ""} onChange={handleChange} onBlur={trimOnBlur}/>
             </FormControl>
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <FormControl sx={{mt: 2, width: 65/100 }} variant="outlined">
               <InputLabel htmlFor="OI">Operator Institution</InputLabel>
               <OutlinedInput id="OI" type="text" name="OpInst" label="Operator Institution" value={inputs.OpInst || ""} onChange={handleChange} onBlur={trimOnBlur}/>
             </FormControl>
           </div>
 
           <div>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <FormControl sx={{ mt: 2, width: 65/100 }} variant="outlined">
               <InputLabel htmlFor="IA">Institution Address</InputLabel>
               <OutlinedInput id="IA" type="text" name="InstAddr" label="Institution Address" value={inputs.InstAddr || ""} onChange={handleChange} onBlur={trimOnBlur}/>
             </FormControl>
